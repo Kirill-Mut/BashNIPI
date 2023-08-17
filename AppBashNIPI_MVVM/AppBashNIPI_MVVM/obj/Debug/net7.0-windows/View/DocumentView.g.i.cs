7a431680fb0b80bcd9c76c6fdbff3d9e24c29567
Damock <141475901+Kirill-Mut@users.noreply.github.com>
2023-08-17 23:13:25 +0500

    AppBashNIPI

--- AppBashNIPI_MVVM/AppBashNIPI_MVVM/obj/Debug/net7.0-windows/View/DocumentView.g.i.cs
+++ AppBashNIPI_MVVM/AppBashNIPI_MVVM/obj/Debug/net7.0-windows/View/DocumentView.g.i.cs
@@ -53,7 +53,7 @@ namespace AppBashNIPIMVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AppBashNIPI_MVVM;component/view/documentview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AppBashNIPI_MVVM;V1.0.0.0;component/view/documentview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\DocumentView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- AppBashNIPI_MVVM/AppBashNIPI_MVVM/obj/Debug/net7.0-windows/View/MissionView.g.i.cs
+++ AppBashNIPI_MVVM/AppBashNIPI_MVVM/obj/Debug/net7.0-windows/View/MissionView.g.i.cs
@@ -55,7 +55,7 @@ namespace AppBashNIPIMVVM.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AppBashNIPI_MVVM;component/view/missionview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AppBashNIPI_MVVM;V1.0.0.0;component/view/missionview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\MissionView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
